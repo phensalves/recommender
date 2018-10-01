71077c8fda5ec56f6b71020d8990da314c37ba69
Pedro Alves <pedro_alves@me.com>
2018-10-01 20:20:40 -0300

    wip changing query

--- pdh_elasticsearch.py
+++ pdh_elasticsearch.py
@@ -1,4 +1,6 @@
+# coding=utf-8
 from elasticsearch import Elasticsearch as es
+from elasticsearch_dsl import Search as s
 
 
 class PdhElasticsearch():
@@ -9,38 +11,21 @@ class PdhElasticsearch():
         if result is not None:
             return result['_source']
 
-    def get_similar_users(self, index, user_preferences):
-        res = es.search(index=index, body={"query": {
-            "function_score": {
-                "boost_mode": "replace",
-                "query": {"match_all": {}},
-                "script_score": {
-                    "script": " _source['interests_ids_array'].containsAll( #{preferences} ) ? 2.3 : 0 "
+    def get_similar_users(self, index, user_preferences, lat, lng):
+        preferences = 
+        res = client.search(index="user_preferences", body={
+            "query": {
+                "function_score": {
+                    "boost_mode": "replace",
+                    "query": {"match_all": {}},
+                    "script_score": {
+                        "script": " _source['interests_ids_array'].containsAll( [19, 20, 21, 22] ) ? 2.3: 0"
+                    }
                 }
             }
-        },
-            "filter": {
-                "bool": {
-                    "must": [
-                        {
-                            "geo_distance": {
-                                "distance": "50km",
-                                "location": {
-                                    "lat": "#{options[:lat]}",
-                                    "lon": "#{options[:lon]}",
-                                }
-                            }
-                        },
-                        {
-                            "terms": {
-                                "interests_ids_array": preferences
-                            }
-                        }
-                    ]
-                }
-            }
+
         })
 
-        print("Got %d Hits:" % res['hits']['total'])
-        for hit in res['hits']['hits']:
-            print(hit["_source"])
+    print("Got %d Hits:" % res['hits']['total'])
+    for hit in res['hits']['hits']:
+        print(hit["_source"])
